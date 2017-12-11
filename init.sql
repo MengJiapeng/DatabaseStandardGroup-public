4f8d7bc74b0b3eb8ae928b89fe306a97ba04d78c
badcode <mjp1753@163.com>
2017-12-11 15:38:22 +0800

    update init.sql

--- init.sql
+++ init.sql
@@ -1,15 +1,15 @@
-create table school
-(
+drop table if exists school;
+create table school(
     id  bigint unsigned primary key auto_increment,
     gmt_create datetime default current_timestamp,
     gmt_modified datetime default current_timestamp on update current_timestamp,
     school_name varchar(20) not null,
     province varchar(10) not null,
     city varchar(10) not null
-);
+)default charset=utf8;
 
-create table user_info
-(
+drop table if exists user_info;
+create table user_info(
     id bigint unsigned primary key auto_increment,
     gmt_create datetime default current_timestamp,
     gmt_modified datetime default current_timestamp on update current_timestamp,
@@ -24,10 +24,10 @@ create table user_info
     number varchar(20),
     education tinyint(1) unsigned ,
     email varchar(50)
-);
+)default charset=utf8;
 
-create table course
-(
+drop table if exists course;
+create table course(
     id bigint unsigned primary key auto_increment,
     gmt_create datetime default current_timestamp,
     gmt_modified datetime default current_timestamp on update current_timestamp,
@@ -37,133 +37,134 @@ create table course
     teacher_id bigint unsigned,
     description varchar(50),
     score_rule_id bigint unsigned
-);
+)default charset=utf8;
 
-DROP TABLE IF EXISTS `score_rule`;
-CREATE TABLE `score_rule` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `gmt_create` datetime DEFAULT current_timestamp,
-  `gmt_modified` datetime DEFAULT current_timestamp ON UPDATE current_timestamp,
-  `report` tinyint(3) unsigned DEFAULT NULL,
-  `five_point_percentage` tinyint(3) unsigned DEFAULT NULL,
-  `four_point_percentage` tinyint(3) unsigned DEFAULT NULL,
-  `three_point_percentage` tinyint(3) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+drop table if exists `score_rule`;
+create table `score_rule` (
+  `id` bigint(20) unsigned not null auto_increment,
+  `gmt_create` datetime default current_timestamp,
+  `gmt_modified` datetime default current_timestamp on update current_timestamp,
+  `report` tinyint(3) unsigned default null,
+  `five_point_percentage` tinyint(3) unsigned default null,
+  `four_point_percentage` tinyint(3) unsigned default null,
+  `three_point_percentage` tinyint(3) unsigned default null,
+  primary key (`id`)
+)default charset=utf8;
 
-DROP TABLE IF EXISTS `class_info`;
-CREATE TABLE `class_info` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `gmt_create` datetime DEFAULT current_timestamp,
-  `gmt_modified` datetime DEFAULT current_timestamp ON UPDATE current_timestamp,
-  `class_name` varchar(255) NOT NULL,
+drop table if exists `class_info`;
+create table `class_info` (
+  `id` bigint(20) unsigned not null auto_increment,
+  `gmt_create` datetime default current_timestamp,
+  `gmt_modified` datetime default current_timestamp on update current_timestamp,
+  `class_name` varchar(255) not null,
   `course_id` bigint(20) unsigned,
-  `site` varchar(255) DEFAULT NULL,
-  `class_time` varchar(255) DEFAULT NULL,
+  `site` varchar(255) default null,
+  `class_time` varchar(255) default null,
   `score_rule_id` bigint(20) unsigned,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  primary key (`id`)
+)default charset=utf8;
 
-DROP TABLE IF EXISTS `course_selection`;
-CREATE TABLE `course_selection` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `gmt_create` datetime DEFAULT current_timestamp,
-  `gmt_modified` datetime DEFAULT current_timestamp ON UPDATE current_timestamp,
+drop table if exists `course_selection`;
+create table `course_selection` (
+  `id` bigint(20) unsigned not null auto_increment,
+  `gmt_create` datetime default current_timestamp,
+  `gmt_modified` datetime default current_timestamp on update current_timestamp,
   `class_id` bigint(20) unsigned,
   `student_id` bigint(20) unsigned,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  primary key (`id`)
+)default charset=utf8;
 
-DROP TABLE IF EXISTS attendance;
-CREATE TABLE attendance(
-    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    gmt_create DATETIME DEFAULT CURRENT_TIMESTAMP,
-    gmt_modified DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    student_id BIGINT UNSIGNED NOT NULL,
-    class_id BIGINT UNSIGNED NOT NULL,
-    attendance_status TINYINT UNSIGNED DEFAULT 0,
-    CHECK (attendance_status=0 || attendance_status=1)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+drop table if exists attendance;
+create table attendance(
+    id bigint unsigned auto_increment primary key,
+    gmt_create datetime default current_timestamp,
+    gmt_modified datetime default current_timestamp on update current_timestamp,
+    student_id bigint unsigned not null,
+    class_id bigint unsigned not null,
+    attendance_status tinyint unsigned default 0,
+    check (attendance_status=0 || attendance_status=1)
+)default charset=utf8;
 
-DROP TABLE IF EXISTS seminar;
-CREATE TABLE seminar(
-    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    gmt_create DATETIME DEFAULT CURRENT_TIMESTAMP,
-    gmt_modified DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    seminar_name VARCHAR(300) NOT NULL,
-    seminar_description VARCHAR(5000) DEFAULT NULL,
-    course_id BIGINT UNSIGNED,
-    is_fixed TINYINT UNSIGNED DEFAULT 0,
-    start_time DATE NOT NULL,
-    end_time DATE NOT NULL,
-    CHECK (is_fixed=0 || is_fixed=1)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+drop table if exists seminar;
+create table seminar(
+    id bigint unsigned auto_increment primary key,
+    gmt_create datetime default current_timestamp,
+    gmt_modified datetime default current_timestamp on update current_timestamp,
+    seminar_name varchar(300) not null,
+    seminar_description varchar(5000) default null,
+    course_id bigint unsigned,
+    is_fixed tinyint unsigned default 0,
+    start_time date not null,
+    end_time date not null,
+    check (is_fixed=0 || is_fixed=1)
+)default charset=utf8;
 
-DROP TABLE IF EXISTS topic;
-CREATE TABLE topic(
-    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    gmt_create DATETIME DEFAULT CURRENT_TIMESTAMP,
-    gmt_modified DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    topic_name VARCHAR(300) NOT NULL,
-    description VARCHAR(5000) DEFAULT NULL,
-    group_number_limit TINYINT UNSIGNED NOT NULL,
-    group_student_limit TINYINT UNSIGNED NOT NULL,
-    seminar_id BIGINT UNSIGNED NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+drop table if exists topic;
+create table topic(
+    id bigint unsigned auto_increment primary key,
+    gmt_create datetime default current_timestamp,
+    gmt_modified datetime default current_timestamp on update current_timestamp,
+    topic_name varchar(300) not null,
+    description varchar(5000) default null,
+    group_number_limit tinyint unsigned not null,
+    group_student_limit tinyint unsigned not null,
+    seminar_id bigint unsigned not null
+)default charset=utf8;
 
-drop table if exists fix_group
-create Table fix_group(
+drop table if exists fix_group;
+create table fix_group(
        id bigint primary key  auto_increment,
        gmt_create datetime default current_timestamp, 
 	   gmt_modified datetime default current_timestamp on update current_timestamp,
        class_id bigint unsigned
-);
+)default charset=utf8;
 
-drop table if exists fix_group_member
-create Table fix_group_member(
+drop table if exists fix_group_member;
+create table fix_group_member(
        id bigint primary key  auto_increment,
        gmt_create datetime default current_timestamp, 
 	   gmt_modified datetime default current_timestamp on update current_timestamp,
        fix_group_id bigint unsigned,
        student_id bigint unsigned,
        leader bigint unsigned  
-);
-drop table if exists seminar_group
-create Table seminar_group(
+)default charset=utf8;
+
+drop table if exists seminar_group;
+create table seminar_group(
        id bigint primary key  auto_increment,
        gmt_create datetime default current_timestamp, 
 	   gmt_modified datetime default current_timestamp on update current_timestamp,
        seminar_id bigint unsigned,
        report_garde int unsigned check(report_grade>=1 and grade<=5),
        report varchar(5000)
-);
+)default charset=utf8;
 
-drop table if exists seminar_group_member
-create Table seminar_group_member(
+drop table if exists seminar_group_member;
+create table seminar_group_member(
        id bigint primary key  auto_increment,
        gmt_create datetime default current_timestamp, 
 	   gmt_modified datetime default current_timestamp on update current_timestamp,
        seminar_group_id bigint unsigned,
        student_id bigint unsigned,
        is_leader tinyint unsigned check(is_leader>=0 and is_leader<=1)
-);
+)default charset=utf8;
 
-drop table if exists seminar_group_topic
-create Table seminar_group_topic(
+drop table if exists seminar_group_topic;
+create table seminar_group_topic(
        id bigint primary key  auto_increment,
        gmt_create datetime default current_timestamp, 
 	   gmt_modified datetime default current_timestamp on update current_timestamp,
        topic_id bigint unsigned,
        seminar_group_id bigint unsigned,
        presentation_grade int unsigned
-);
+)default charset=utf8;
 
-drop table if exists student_score_group
-create Table student_score_group(
+drop table if exists student_score_group;
+create table student_score_group(
        id bigint primary key  auto_increment,
        gmt_create datetime default current_timestamp, 
 	   gmt_modified datetime default current_timestamp on update current_timestamp,
        student_id bigint unsigned,
        seminar_group_topic bigint unsigned,
        grade tinyint unsigned check(grade>=1 and grade<=5)
-);
+)default charset=utf8;
